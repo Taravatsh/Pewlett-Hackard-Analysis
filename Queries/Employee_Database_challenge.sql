3dad24498947fee1978c764029107ae506a65d4d
Taravat <eau0715362@gmail.com>
2021-05-30 14:22:18 -0700

    Queries for Deliverable 1 added.

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -35,7 +35,7 @@ SELECT em.emp_no,
 	ti.to_date
 INTO retirement_titles
 FROM employees as em
-LEFT JOIN titles as ti
+INNER JOIN titles as ti
 ON em.emp_no = ti.emp_no
 WHERE em.birth_date BETWEEN '1952-01-01' AND '1955-12-31'
 ORDER BY em.emp_no;
@@ -48,3 +48,10 @@ rt.title
 INTO unique_titles
 FROM retirement_titles as rt
 ORDER BY emp_no, to_date DESC;
+
+-- Retrieve the number of employees by their most recent title
+SELECT COUNT(emp_no), title
+INTO retiring_titles
+FROM unique_titles
+GROUP BY title 
+ORDER BY COUNT DESC;
