d69eb01493395b6667c77db712a53866a7f71779
Taravat <eau0715362@gmail.com>
2021-05-30 23:02:59 -0700

    Additional queries.

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -1,3 +1,11 @@
+-- Creating Departments Table
+CREATE TABLE departments (
+	dept_no VARCHAR(4) NOT NULL,
+	dept_name VARCHAR NOT NULL,
+	PRIMARY KEY (dept_no),
+	UNIQUE (dept_name)
+);
+
 -- Creating Employees Table 
 CREATE TABLE employees (
 	emp_no INT NOT NULL,
@@ -31,6 +39,7 @@ CREATE TABLE dept_emp (
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no), 
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
 	PRIMARY KEY (emp_no, dept_no)
 );
 
@@ -82,7 +91,7 @@ SELECT DISTINCT ON (em.emp_no) em.emp_no,
 	de.from_date,
 	de.to_date,
 	ti.title
--- INTO mentorship_eligibilty
+INTO mentorship_eligibility
 FROM employees as em
 INNER JOIN dept_emp as de
 ON em.emp_no = de.emp_no
@@ -92,7 +101,56 @@ WHERE de.to_date = ('9999-01-01')
 AND em.birth_date BETWEEN '1965-01-01' AND '1965-12-31'
 ORDER BY em.emp_no;
 
+-- Additional queries that may provide more insight into the upcoming "silver tsunami"
+SELECT COUNT(emp_no) 
+FROM unique_titles;
+----
+-- Retrieving the number of employees eligible for mentorship
+SELECT DISTINCT ON (ut.emp_no) ut.emp_no,
+d.dept_name
+INTO mentorship
+FROM unique_titles as ut
+INNER JOIN dept_emp as de
+ON (ut.emp_no = de.emp_no)
+INNER JOIN departments as d
+ON (de.dept_no = d.dept_no);
+
+SELECT COUNT(emp_no), dept_name
+FROM mentorship
+GROUP BY dept_name
+ORDER BY COUNT DESC;
+
+-- Retrieving the number of employees eligible for mentorship
+SELECT COUNT(emp_no), title
+-- INTO mentorship_counts
+FROM mentorship_eligibility 
+GROUP BY title
+ORDER BY COUNT(emp_no) DESC;
 
 
-	
+-- Retrieving the number of employees eligible for mentorship
+SELECT COUNT(me.emp_no), d.dept_name
+-- INTO mentorship_counts
+FROM mentorship_eligibility as me
+INNER JOIN dept_emp as de
+ON (me.emp_no = de.emp_no)
+INNER JOIN departments as d
+ON (de.dept_no = d.dept_no)
+GROUP BY dept_name
+ORDER BY COUNT(me.emp_no) DESC;
+--
 
+-- Retrieving the number of employees eligible for mentorship
+SELECT DISTINCT ON (me.emp_no) d.dept_name,
+	me.emp_no
+INTO mentorship_counts
+FROM mentorship_eligibility as me
+INNER JOIN dept_emp as de
+ON (me.emp_no = de.emp_no)
+INNER JOIN departments as d
+ON (de.dept_no = d.dept_no);
+
+SELECT COUNT(emp_no), dept_name
+FROM mentorship_counts
+GROUP BY dept_name
+ORDER BY COUNT DESC;
