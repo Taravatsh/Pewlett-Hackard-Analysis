b2850e47089d7053a6b14411922b91ae2477449b
Taravat <eau0715362@gmail.com>
2021-05-30 17:15:47 -0700

    Queries of deliverable 2 added.

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -1,4 +1,4 @@
--- Create Employees Table 
+-- Creating Employees Table 
 CREATE TABLE employees (
 	emp_no INT NOT NULL,
 	birth_date DATE NOT NULL,
@@ -11,7 +11,7 @@ CREATE TABLE employees (
 -- Confirming Employees table has been imported 
 SELECT * FROM employees;
 
--- Create Titles Table 
+-- Creating Titles Table 
 CREATE TABLE titles (
 	emp_no INT NOT NULL,
 	title VARCHAR(50) NOT NULL,
@@ -24,9 +24,23 @@ CREATE TABLE titles (
 -- Confirming Titles Table has been imported
 SELECT * FROM titles;
 
+-- Creating Department Employee Table
+CREATE TABLE dept_emp (
+	emp_no INT NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no), 
+	PRIMARY KEY (emp_no, dept_no)
+);
+
+-- Confirming Titles Table has been imported
+SELECT * FROM dept_emp;
+
 -- Deliverable 1: The Number of Retiring Employees by Title
 
--- Creating new table for retirement titles 
+-- Creating a Retirement Titles table that holds all the titles
+-- of current employees who were born between January 1, 1952 and December 31, 1955
 SELECT em.emp_no,
 	em.first_name,
 	em.last_name,
@@ -41,17 +55,44 @@ WHERE em.birth_date BETWEEN '1952-01-01' AND '1955-12-31'
 ORDER BY em.emp_no;
 
 -- Use Dictinct with Orderby to remove duplicate rows
-SELECT DISTINCT ON (emp_no) rt.emp_no,
+SELECT DISTINCT ON (rt.emp_no) rt.emp_no,
 rt.first_name,
 rt.last_name,
 rt.title
 INTO unique_titles
 FROM retirement_titles as rt
-ORDER BY emp_no, to_date DESC;
+ORDER BY emp_no ASC, to_date DESC;
 
 -- Retrieve the number of employees by their most recent title
 SELECT COUNT(emp_no), title
 INTO retiring_titles
 FROM unique_titles
 GROUP BY title 
-ORDER BY COUNT DESC;
+ORDER BY COUNT(emp_no) DESC;
+
+-- Deliverable 2: The Employees Eligible for the Mentorship Program
+
+-- Creating a Mentorship Eligibility table that holds the employees
+-- who are eligible to participate in a mentorship program
+
+SELECT DISTINCT ON (em.emp_no) em.emp_no,
+	em.first_name,
+	em.last_name,
+	em.birth_date,
+	de.from_date,
+	de.to_date,
+	ti.title
+-- INTO mentorship_eligibilty
+FROM employees as em
+INNER JOIN dept_emp as de
+ON em.emp_no = de.emp_no
+INNER JOIN titles as ti
+ON em.emp_no = ti.emp_no
+WHERE de.to_date = ('9999-01-01')
+AND em.birth_date BETWEEN '1965-01-01' AND '1965-12-31'
+ORDER BY em.emp_no;
+
+
+
+	
+
