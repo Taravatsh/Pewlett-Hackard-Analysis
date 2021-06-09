b16e85fc8586cccdf45523da34b000deaf1c12a3
Taravat <eau0715362@gmail.com>
2021-06-09 16:47:37 -0700

    Additional queries for deliverable 3.

--- Queries/Additional_Queries_deliverable_3.sql
+++ Queries/Additional_Queries_deliverable_3.sql
@@ -1,6 +1,6 @@
 -- Deliverable 3: Additional queries that may provide more insight into the upcoming "silver tsunami".
 
--- Use Distinct ON with Order By to remove duplicate rows.
+-- Use Distinct ON with Orderby to remove duplicate rows.
 SELECT DISTINCT ON (ut.emp_no) ut.emp_no,
 	d.dept_name
 INTO roles_dept
@@ -11,14 +11,14 @@ INNER JOIN departments as d
 ON (de.dept_no = d.dept_no)
 ORDER BY (ut.emp_no);
 
--- Retrieving the number of roles that need to be filled in each department.
+-- Retrieve the number of roles that need to be filled in each department.
 SELECT COUNT(emp_no), dept_name
 INTO roles_to_be_filled
 FROM roles_dept
 GROUP BY (dept_name)
 ORDER BY COUNT(emp_no) DESC;
 
--- Use Distinct ON with Order By to remove duplicate rows.
+-- Use Distinct ON with Orderby to remove duplicate rows.
 SELECT DISTINCT ON (me.emp_no) me.emp_no,
 	d.dept_name
 INTO mentors_dept
@@ -29,7 +29,7 @@ INNER JOIN departments as d
 ON (de.dept_no = d.dept_no)
 ORDER BY (me.emp_no);
 
--- Retrieving the number of retirement-ready employees in the departments eligible for mentorship.
+-- Retrieve the number of retirement-ready employees in the departments eligible for mentorship.
 SELECT COUNT(emp_no), dept_name
 INTO mentors_retirement_dept
 FROM mentors_dept
